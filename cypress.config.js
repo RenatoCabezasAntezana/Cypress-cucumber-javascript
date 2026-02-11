b1208c5f7ef52cd45d1c2ea2e29e64ca409ddb2a
RenatoCabezasAntezana <renato.sistemas02@gmail.com>
2026-02-10 19:05:20 -0500

    test

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,11 @@
 const { defineConfig } = require("cypress");
 const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
-const { addCucumberPreprocessorPlugin } = require("@badeball/cypress-cucumber-preprocessor");
-const { createEsbuildPlugin } = require("@badeball/cypress-cucumber-preprocessor/esbuild");
+const {
+  addCucumberPreprocessorPlugin,
+} = require("@badeball/cypress-cucumber-preprocessor");
+const {
+  createEsbuildPlugin,
+} = require("@badeball/cypress-cucumber-preprocessor/esbuild");
 
 module.exports = defineConfig({
   waitForAnimations: false,
@@ -10,7 +14,12 @@ module.exports = defineConfig({
   e2e: {
     specPattern: "cypress/e2e/features/*.feature",
     baseUrl: "https://demoqa.com/automation-practice-form",
-
+    reporter: "cypress-junit-reporter",
+    reporterOptions: {
+      mochaFile: "cypress/results/results-[hash].xml", // Estructura del nombre
+      toConsole: true, // Para ver el progreso en la terminal
+      attachments: true,
+    }, // Si quieres que Xray intente vincular pantallazos
     async setupNodeEvents(on, config) {
       // 1. PRIMERO: Registrar el plugin de Cucumber
       await addCucumberPreprocessorPlugin(on, config);
@@ -20,11 +29,11 @@ module.exports = defineConfig({
         "file:preprocessor",
         createBundler({
           plugins: [createEsbuildPlugin(config)],
-        })
+        }),
       );
 
       // 3. MUY IMPORTANTE: Retornar el objeto config modificado
-      return config; 
+      return config;
     },
-  }, 
+  },
 });
